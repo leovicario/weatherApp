701c76760691aa355c0af81ff8828b5d641bc6fe
leo <65235570+leovicario@users.noreply.github.com>
2020-08-09 22:37:48 +0200

    Weather App 0.1
    First version with functional city searching, basic design and a not-so-slick UI.

---
+++ precache-manifest.2bf2a12cbea6f069afde7140be50ee1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e0111468599b0bc560165d1e568c22",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59435b00a06aa8a9e223",
+    "url": "./static/css/main.664162f3.chunk.css"
+  },
+  {
+    "revision": "e5e35b32211091d9221b",
+    "url": "./static/js/2.a59ad0ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a59ad0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59435b00a06aa8a9e223",
+    "url": "./static/js/main.67d12d19.chunk.js"
+  },
+  {
+    "revision": "eeaf9a9d20c8f1ba0200",
+    "url": "./static/js/runtime-main.a53f7f15.js"
+  },
+  {
+    "revision": "9836b0bb9a2dc65c77a2eaee55668ca6",
+    "url": "./static/media/bg.9836b0bb.jpg"
+  }
+]);
